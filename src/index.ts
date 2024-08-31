0d5bbd7762945aedc28f45b5067c618143ce6a80
Shinya Fujino <shf0811@gmail.com>
2024-08-31 22:52:00 +0900

    hmm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { rule as banNullpo } from "./rules/ban-nullpo.js";
 
 const plugin = {
+	configs: {
+		get recommended() {
+			return recommended;
+		},
+	},
 	rules: {
 		"ban-nullpo": banNullpo,
+	},
+};
+
+const recommended = {
+	plugins: {
+		nullpo: plugin,
+	},
+	rules: {
+		"nullpo/ban-nullpo": "error",
 	},
 };
 
