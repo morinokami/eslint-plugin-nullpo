4267d9464b0579394ce747a117c820171fc33903
Shinya Fujino <shf0811@gmail.com>
2024-08-31 20:59:06 +0900

    Add recommended config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { rule as banNullpo } from "./rules/ban-nullpo.js";
 
-export default {
+const plugin = {
+	configs: {
+		get recommended() {
+			return recommended;
+		},
+	},
 	rules: {
 		"ban-nullpo": banNullpo,
 	},
 };
+
+const recommended = {
+	plugins: {
+		nullpo: plugin,
+	},
+	rules: {
+		"nullpo/ban-nullpo": "error",
+	},
+};
+
+export default plugin;
