ac40e364e277a19281baa33225d0dbce70f10c7b
Shinya Fujino <shf0811@gmail.com>
2024-09-02 23:45:42 +0900

    plugin.meta (#12)
    * plugin.meta
    
    * Create small-monkeys-shave.md

---
+++ .changeset/small-monkeys-shave.md
@@ -0,0 +1,5 @@
+---
+"eslint-plugin-nullpo": patch
+---
+
+plugin.meta
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import fs from "node:fs";
+
 import { rule as noNullpo } from "./rules/no-nullpo.js";
+
+const pkg = JSON.parse(
+	fs.readFileSync(new URL("../package.json", import.meta.url), "utf8"),
+);
 
 const plugin = {
 	configs: {
 		get recommended() {
 			return recommended;
 		},
+	},
+	meta: {
+		name: pkg.name,
+		version: pkg.version,
 	},
 	rules: {
 		"no-nullpo": noNullpo,
